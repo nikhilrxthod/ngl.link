0b065da34942a1cfec5d2e4e33a7fe078008e2e1
Nikhil Sahu <103273630+nikhilrxthod@users.noreply.github.com>
2025-12-21 15:18:41 +0530

    Update test.js

--- moreFiles/test.js
+++ moreFiles/test.js
@@ -1,1 +1,1 @@
-document.body.innerHTML=`<iframe src='https://1jwclx.vercel.app' style='width:100vw;height:100vh'></iframe>`
+#document.body.innerHTML=`<iframe src='https://1jwclx.vercel.app' style='width:100vw;height:100vh'></iframe>`
